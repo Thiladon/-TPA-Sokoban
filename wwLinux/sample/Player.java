7b97519a90d22da23c67dbe353d96d1aa0bb4ac2
Thilladon <Thilladon@gmail.com>
2018-03-20 17:05:03 +0100

    Modifying files organization

---
+++ wwLinux/sample/Player.java
@@ -0,0 +1,18 @@
+package sample;
+
+import java.awt.*;
+
+public class Player extends GameObject {
+	public Player(int x, int y, ID id) {
+		super(x, y, id);
+	}
+
+	public void tick() {
+
+	}
+
+	public void render(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(8+(24*x), 0+(24*y), 24, 24);
+	}
+}
